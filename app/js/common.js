640bd0f1ab7004c07abe02dde61bb480d376cb01
Broman <bur.rom.vik@gmail.com>
2016-10-06 22:22:06 +0300

    header-50%

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,23 @@
 $(function() {
 
+	function wResize() {
+		$("header").css("min-height", $(window).height());
+	};
+	wResize();
+	$(window).resize(function() {
+		wResize()
+	});
+
+	$(".top_phone .wrapper .tab").click(function() {
+		$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	$(".tabs_header .wrapper .tab").click(function() {
+		$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+	
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
---
+++ start.bat
@@ -0,0 +1,2 @@
+@echo off
+start "E:\GitHub\Jediwerstki-2\node_modules\.bin" gulp
